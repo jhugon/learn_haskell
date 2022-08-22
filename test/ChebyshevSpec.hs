e8e1f6e82e0960de6e6be973bbb1f0a06b7d55b3
Justin Hugon <jhugon@users.noreply.github.com>
2022-08-22 16:44:47 -0500

    test: added laguerre tests
    resolves #2

--- test/ChebyshevSpec.hs
+++ test/ChebyshevSpec.hs
@@ -2,6 +2,10 @@ module ChebyshevSpec (spec) where
 import SpecHelper
 import Chebyshev
 
+floatingEqualsMaybe :: Maybe Double -> Double -> Maybe Bool
+floatingEqualsMaybe (Just x) y = Just ((x-y) < 1e-6)
+floatingEqualsMaybe Nothing _ = Nothing
+
 spec :: Spec
 spec = do
   describe "factorial function" $ do
@@ -80,3 +84,27 @@ spec = do
       (chebyshev2nd 10000 (-1)) `shouldBe` Nothing
     it "can't compute the x = 0, n = -1000 case" $ do
       (chebyshev2nd 0 (-1000)) `shouldBe` Nothing
+  describe "laguerre function" $ do
+    it "can compute the x = 0, n = 0 case" $ do
+      (laguerre 0 0) `shouldBe` Just 1
+    it "can compute the x = 1000, n = 0 case" $ do
+      (laguerre 1000 0) `shouldBe` Just 1
+    it "can compute the x = -1000, n = 0 case" $ do
+      (laguerre (-1000) 0) `shouldBe` Just 1
+    it "can compute the x = 0, n = 1 case" $ do
+      (laguerre 0 1) `shouldBe` Just 1
+    it "can compute the x = 1000, n = 1 case" $ do
+      (laguerre 1000 1) `shouldBe` Just (-999)
+    it "can compute the x = -1000, n = 1 case" $ do
+      (laguerre (-1000) 1) `shouldBe` Just 1001
+    it "can compute the x = 0, n = 10 case" $ do
+      (laguerre 0 10) `shouldBe` Just 1
+    it "can compute the x = 10, n = 10 case" $ do
+      --(<1e-8) <$> (+(-1763.0)/63.0)  <$> (laguerre 10 10) `shouldBe` Just True
+      floatingEqualsMaybe (laguerre 10 10) (1763.0/63.0) `shouldBe` Just True
+    it "can't compute the x = -5, n = -1 case" $ do
+      (laguerre (-5) (-1)) `shouldBe` Nothing
+    it "can't compute the x = 10000, n = -1 case" $ do
+      (laguerre 10000 (-1)) `shouldBe` Nothing
+    it "can't compute the x = 0, n = -1000 case" $ do
+      (laguerre 0 (-1000)) `shouldBe` Nothing
