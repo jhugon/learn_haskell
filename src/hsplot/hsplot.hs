5f6e27fd3391dc12adce8591d3261a33dd48b0fb
Justin Hugon <jhugon@users.noreply.github.com>
2025-02-01 16:35:03 -0600

    got command line argument working with hsplot

--- src/hsplot/hsplot.hs
+++ src/hsplot/hsplot.hs
@@ -2,9 +2,22 @@ module Main where
 
 import TerminalPlotter
 import TSVParser
+import Options.Applicative
 
 default (Int, Float)
 
+data CmdOptions = CmdOptions { filename :: String }
+
+cmdopts :: Parser CmdOptions
+cmdopts = CmdOptions <$> argument str ( metavar "FILENAME" <> help "Input file name" )
+
 main :: IO ()
-main = do
-    runScatterPlotFromFile "testdata.txt"
+main = run =<< execParser opts
+    where
+        opts = info (cmdopts <**> helper)
+            ( fullDesc
+            <> progDesc "A scatter plot of each line in FILENAME, where each line is like \"1.0 5.6\""
+            <> header "hsplot - a Haskell fixed-width text plotter" )
+
+run :: CmdOptions -> IO ()
+run ( CmdOptions fn ) = runScatterPlotFromFile fn
